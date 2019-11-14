bc19d69c4c6d8fa13e6fac3a324e84c1c98e6f24
mhj <787597538@qq.com>
2019-11-14 23:44:49 +0800

    mhj2

---
+++ src/test/test/A.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.Queue;
+
+public class A {
+    static int a=1;
+    public A(){
+        PrintFields();
+    }
+    public void PrintFields(){}
+    interface a{
+
+    }
+}
